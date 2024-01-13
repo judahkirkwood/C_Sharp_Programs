7171a734642e3f24809ffb478684627b3f7d142a
judahkirkwood <judahkirkwood@gmail.com>
2024-01-13 01:25:06 -0800

    C# Daily Report Program
    Basic Daily Report written in C#

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy.");
+            Console.WriteLine("Student Daily Report.");
+            Console.ReadLine();
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+
+            Console.Write("What course are you on? ");
+            string courseName = Console.ReadLine();
+
+            // Ask for the page number
+            Console.Write("What page number? ");
+            // Read and parse the page number as an integer
+            if (int.TryParse(Console.ReadLine(), out int pageNumber))
+            {
+                // Successfully parsed as an integer
+                // Ask if the user needs help
+                Console.Write("Do you need help with anything? Please answer \"true\" or \"false\": ");
+
+                // Read and parse the help status as a boolean
+                if (bool.TryParse(Console.ReadLine(), out bool needsHelp))
+                {
+                    // Successfully parsed as a boolean
+                    // Ask for positive experiences
+                    Console.Write("Were there any positive experiences you’d like to share? Please give specifics: ");
+                    string positiveExperiences = Console.ReadLine();
+
+                    // Ask for additional feedback
+                    Console.Write("Is there any other feedback you’d like to provide? Please be specific: ");
+                    string additionalFeedback = Console.ReadLine();
+
+                    // Ask for the number of hours studied
+                    Console.Write("How many hours did you study today? ");
+
+                    // Read and parse the hours studied as a double
+                    if (double.TryParse(Console.ReadLine(), out double hoursStudied))
+                    {
+                        // Successfully parsed as a double
+
+                        // Display a closing message
+                        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+                    }
+
+
+
+                    // Display a personalized greeting
+                    Console.WriteLine($"Have a great day, {userName}!");
+
+                    Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            // (To "comment out" code, highlight and press CTRL+K+C)
+            // (To remove comments, highlight "commented code" and press CTRL+K+U)
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is yout favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = true;
+            byte hoursWorked = 45;
+            sbyte currentTemperature = -13;
+            char questionMark = '\u2103';
+            decimal moneyInBank = 124000.5m;
+            double heightInCentimeters = 211.30202092;
+            float secondsLeft = 2.62f;
+            short temperatureOnMars = -342;
+            string myName = "Judah";
+
+            int currentAge = 38;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+
+
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+        }
+    }
+}
